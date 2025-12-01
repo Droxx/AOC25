95b1dd324ea2898ba0386287c3c4e449b06bb4c2
Michael Stocks <michaelstocks1@gmail.com>
2025-12-01 11:56:52 +0100

    feat: intiial comit

---
+++ AOC25/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Swagger"));
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/run", (string input) =>
+    {
+        return input;
+    })
+    .WithName("RunDay");
+
+app.Run();
