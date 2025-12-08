4974dc886bdcc636eacdf9c51ca296c802f36b5d
Michael Stocks <michaelstocks1@gmail.com>
2025-12-08 11:23:24 +0100

    day 6

--- AOC25/Days/Day6.cs
+++ AOC25/Days/Day6.cs
@@ -46,39 +46,61 @@ public class Day6 : IDay
     private List<Problem> ParseProblemsPart2(string input)
     {
         var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
-        var width = lines.Max(l=>l.Length);
-        var columns = width / ColumnWidth;
-        
-        var problems = new Problem[columns];
-        
-        // For each column
-        for(int c = 0; c < columns; c++)
+        var matrix = new string[lines.Length, lines.Max(l => l.Length)]; // y,x
+        // populate the matrix
+        for(int y = 0; y < lines.Length; y++)
         {
-            var prob = new Problem();
-            var sbs = new StringBuilder[ColumnWidth];
-            
-            var column = lines.Select(l => l.Substring(c * ColumnWidth, ColumnWidth)).ToArray();
-            
-            // for each line in that column
-            for(int l = ColumnWidth-2; l >= 0; l--)
+            var line = lines[y];
+            for(var x = 0; x < line.Length; x++)
             {
-                sbs[l] = new StringBuilder();
-                for (var r = 0; r < lines.Length - 1; r++)
-                {
-                    var ch = column[r][l].ToString();
-                    if(!string.IsNullOrEmpty(ch))
-                        sbs[l].Append(ch);
-                }
-                prob.Numbers.Add(sbs[l].ToString().Replace(" ", ""));
+                matrix[y, x] = line[x].ToString();
             }
-            
-            var opChar = column[^1].Trim();
-            prob.Operator = opChar == "+" ? Operator.Add : Operator.Multiply;
-
-            problems[c] = prob;
         }
+        
+        var rotated = RotateMatrixCounterClockwise(matrix);
+        
+        var problems = new Problem[input.Count(i => i == '+' || i == '*')];
+        problems[0] = new Problem();
+        
+        var problemCount = 0;
+        for (var row = 0; row < rotated.GetLength(0); row++)
+        {
+            var sb = new StringBuilder();
+            for(var i = 0; i < rotated.GetLength(1)-1; i++)
+            {
+                if(!string.IsNullOrWhiteSpace(rotated[row,i]))
+                    sb.Append(rotated[row, i] ?? " ");
+            }
+            problems[problemCount].Numbers.Add(sb.ToString());
+            
+            if(rotated[row, rotated.GetLength(1)-1] == "+" || rotated[row, rotated.GetLength(1)-1] == "*")
+            {
+                problems[problemCount].Operator = rotated[row, rotated.GetLength(1)-1] == "+" ? Operator.Add : Operator.Multiply;
+                problemCount++;
+                if(problemCount < problems.Length)
+                    problems[problemCount] = new Problem();
+            }
+        }
+        
 
         return problems.ToList();
+    }
+    
+    static string[,] RotateMatrixCounterClockwise(string[,] oldMatrix)
+    {
+        string[,] newMatrix = new string[oldMatrix.GetLength(1), oldMatrix.GetLength(0)];
+        int newColumn, newRow = 0;
+        for (int oldColumn = oldMatrix.GetLength(1) - 1; oldColumn >= 0; oldColumn--)
+        {
+            newColumn = 0;
+            for (int oldRow = 0; oldRow < oldMatrix.GetLength(0); oldRow++)
+            {
+                newMatrix[newRow, newColumn] = oldMatrix[oldRow, oldColumn];
+                newColumn++;
+            }
+            newRow++;
+        }
+        return newMatrix;
     }
     
     private class Problem
@@ -88,7 +110,7 @@ public class Day6 : IDay
         { 
             get
             {
-                return Numbers.Where(n => !string.IsNullOrEmpty(n)).Select(n => long.Parse(n)).ToList();
+                return Numbers.Where(n => !string.IsNullOrWhiteSpace(n)).Select(n => long.Parse(n)).ToList();
             }
         }
         public Operator Operator { get; set; }
